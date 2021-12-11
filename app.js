e1e4d9505249716bbd9645d58f2f83c1724ab134
jgodoyc <jgodoyc@hotmail.com>
2021-12-11 14:39:17 -0300

    Se consume API y muestra datos en tarjetas

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
-fetch('http://127.0.0.1:5000/productos')
-    .then(response => response.json())
-    .then(data => console.log(data));
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData()
+})
+
+const fetchData = async () => {
+    try {
+        const res = await fetch('http://127.0.0.1:5000/productos')
+        const data = await res.json()
+        pintarProductos(data)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const contenedorProductos = document.querySelector('#contenedor-productos')
+const pintarProductos = (data) => {
+    const template = document.querySelector('#template-productos').content
+    const fragment = document.createDocumentFragment()
+
+    data['productos'].forEach(producto => {
+        //console.log(producto.url_image)
+        template.querySelector('img').setAttribute('src', producto.url_image)
+        template.querySelector('h5').textContent = producto.name
+        template.querySelector('p span').textContent = producto.price
+
+        const clone = template.cloneNode(true)
+        fragment.appendChild(clone)
+    });
+
+    contenedorProductos.appendChild(fragment)
+}
