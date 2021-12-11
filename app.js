c10d39442924b72e2d774314cf7ece8a04c64b2f
jgodoyc <jgodoyc@hotmail.com>
2021-12-11 15:44:39 -0300

    Se agrega botonera para categorias

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     fetchData()
+    fetchDataC()
 })
 
 const fetchData = async () => {
@@ -27,6 +28,38 @@ const pintarProductos = (data) => {
         const clone = template.cloneNode(true)
         fragment.appendChild(clone)
     });
-
     contenedorProductos.appendChild(fragment)
 }
+
+
+
+
+const fetchDataC = async () => {
+    try {
+        const resC = await fetch('http://127.0.0.1:5000/categorias')
+        const dataC = await resC.json()
+        pintarCategorias(dataC)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+const contenedorCategorias = document.querySelector('#contenedor-categorias')
+const pintarCategorias = (dataC) => {
+    const templateC = document.querySelector('#template-categorias').content
+    const fragmentC = document.createDocumentFragment()
+
+    dataC['categorias'].forEach(categoria => {
+        console.log(categoria.name)
+        templateC.querySelector('span').textContent = categoria.name
+        const cloneC = templateC.cloneNode(true)
+        fragmentC.appendChild(cloneC)        
+    });
+    
+    
+    contenedorCategorias.appendChild(fragmentC)
+}
+
+    
