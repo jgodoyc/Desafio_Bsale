e9f80f14a00448fb92d508c8a57d34f668c2522a
jgodoyc <jgodoyc@hotmail.com>
2021-12-11 22:46:06 -0300

    Filtrar por categorias

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     fetchData()
-    fetchDataC()
-    fetchDataf()
+    fetchDataC()    
 })
+
+const contenedorCategorias = document.querySelector('#contenedor-categorias')
+const contenedorProductos = document.querySelector('#contenedor-productos')
+const buscador = document.querySelector('search')
+const productos = []
 
 const fetchData = async () => {
     try {
@@ -15,18 +19,29 @@ const fetchData = async () => {
     }
 }
 
+const fetchDataf = async (idCategoria) => {
+    try {
+        const res = await fetch('http://127.0.0.1:5000/categorias/'+idCategoria)                    
+        const data = await res.json()
+        pintarProductos(data)
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-const contenedorProductos = document.querySelector('#contenedor-productos')
-const pintarProductos = (data) => {
+
+const pintarProductos = (data) => {       
+    contenedorProductos.innerHTML = ""
     const template = document.querySelector('#template-productos').content
     const fragment = document.createDocumentFragment()
-    
-    data['productos'].forEach(producto => {
+
+    data['productos'].forEach(producto => { 
         template.querySelector('img').setAttribute('src', producto.url_image)
         template.querySelector('h5').textContent = producto.name
         template.querySelector('p span').textContent = producto.price
 
-        const clone = template.cloneNode(true)
+        const clone = template.firstElementChild.cloneNode(true)
         fragment.appendChild(clone)        
     });
     contenedorProductos.appendChild(fragment)
@@ -45,7 +60,7 @@ const fetchDataC = async () => {
 }
 
 
-const contenedorCategorias = document.querySelector('#contenedor-categorias')
+
 const pintarCategorias = (dataC) => {
     const templateC = document.querySelector('#template-categorias').content
     const fragmentC = document.createDocumentFragment()
@@ -53,39 +68,22 @@ const pintarCategorias = (dataC) => {
     dataC['categorias'].forEach(categoria => {
         templateC.querySelector('span').textContent = categoria.name
         templateC.querySelector('button').dataset.id = categoria.id
-        const cloneC = templateC.cloneNode(true)
-        fragmentC.appendChild(cloneC)        
-    });    
-    
-    contenedorCategorias.appendChild(fragmentC)
-    const editButtons = document.querySelectorAll('.btn-secondary');
+        const id = categoria.id        
+
+        const cloneC = templateC.firstElementChild.cloneNode(true)
+        cloneC.addEventListener('click', async (id) => {
+            
+            const idCategoria = categoria.id
+            await fetchDataf(idCategoria);
+        })         
+        fragmentC.appendChild(cloneC) 
+    });  
+    contenedorCategorias.appendChild(fragmentC)     
+      
+}
 
 
 
-    editButtons.forEach(function(item, idx){
-
-        item.addEventListener('click', function(){
-            contenedorProductos.remove();
-            const idCategoria = item.dataset.id
-            console.log(idCategoria)
-            fetchDataf();
-        })
-
-        const fetchDataf = async () => {
-            try {
-                const resf = await fetch('http://127.0.0.1:5000/categorias/'+item.dataset.id)                    
-                const dataf = await resf.json()
-                console.log(dataf)
-                pintarProductos(dataf)
-                
-            } catch (error) {
-                console.log(error)
-            }
-        }
-    });
-    
-} 
-
-
-
-
+//onChange
+// .filter
+//productos.filter((producto ) => producto.name.includes("Cadena a buscar"))
